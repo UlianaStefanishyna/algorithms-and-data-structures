cfa90a588ebdef2d37bcd4ed609c9ca33ad25d05
Uliana <ustefs@gmail.com>
2023-01-31 12:33:57 +0000

    Fibonacci sequence

---
+++ src/recursion/get_fibonacci_seq.py
@@ -0,0 +1,21 @@
+"""Implement a function recursively to get the desired
+Fibonacci sequence value.
+Your code should have the same input/output as the
+iterative code in the instructions."""
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+
+def get_fib(position):
+    if position <= 0:
+        return 0
+    elif position == 1:
+        return 1
+    else:
+        return get_fib(position-1) + get_fib(position-2)
+
+
+if __name__ == '__main__':
+    print(get_fib(9))
+    print(get_fib(11))
+    print(get_fib(0))
